bd03c2da1bbc51561e157d82e973b448adbab992
Angel Santana <angel.d.santana00@gmail.com>
2022-03-11 15:27:33 -0600

    saving user input and returning displays

--- src/Contacts.java
+++ src/Contacts.java
@@ -11,8 +11,7 @@ public static void userMenu(){
             "5. Exit\n" +
             "Please Enter an Option | 1 - 5 |");
 
-    int userChoice =
-
+    userOptions();
 }
                         //<============ User Input =============>//
 public static int captureUserInput(){
@@ -22,20 +21,30 @@ public static int captureUserInput(){
 
     if(usersChoice >=1 && usersChoice <=5){
         return usersChoice;
-    }else{
+    } else {
         System.out.println("Please Pick Another Number");
         return captureUserInput();
     }
-
-
-//
 }
 
+                    //<============ userOptions =============>//
+    public static void userOptions() {
+        int choice;
+            do{
+            choice = captureUserInput();
+            if(choice == 1){
+                System.out.println("Viewing all Contacts");
+            }
+
+
+            } while(choice !=5);
+                System.out.println();
+    }
+
+
+
     public static void main(String[] args) {
-
         userMenu();
-        captureUserInput();
-
 
     }
 
@@ -44,6 +53,8 @@ public static int captureUserInput(){
 //TODO: Step 1 : Create the menu / options✅
 // TODO: Step 2: Capture user input✅
 // TODO: Step 3: Lets display something based on the option they choose
+// so we can already capture their input.... if a user picks 1 display "this"; just try writing the method
+// think how can we do that
 
-// 5 minutes left? tf
+// me stuck
 // just start another session after.aight im stuck

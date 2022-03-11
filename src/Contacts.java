c6eb3ec867f780b598446b5535dc6e990dd1c204
Angel Santana <angel.d.santana00@gmail.com>
2022-03-11 14:38:52 -0600

    CLI is up BOI

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,29 +1,49 @@
+import java.util.Scanner;
 
 public class Contacts {
 
-    protected String contactName, phoneNumber;
+                        //<============ Displays CLI =============>//
+public static void userMenu(){
+    System.out.println("1. View Contacts \n" +
+            "2. Add a New Contact\n" +
+            "3. Search For Contact\n" +
+            "4. Delete Existing Contact\n" +
+            "5. Exit\n" +
+            "Please Enter an Option | 1 - 5 |");
 
-    public Contacts(String contactName, String phoneNumber) {
-        this.contactName = contactName;
-        this.phoneNumber = phoneNumber;
+    int userChoice =
+
+}
+                        //<============ User Input =============>//
+public static int captureUserInput(){
+    Scanner in = new Scanner(System.in);
+
+    int usersChoice = in.nextInt();
+
+    if(usersChoice >=1 && usersChoice <=5){
+        return usersChoice;
+    }else{
+        System.out.println("Please Pick Another Number");
+        return captureUserInput();
     }
 
-    //<=============== GETTERS ================>//
-    public String getContactName() {
-        return contactName;
-    }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+//
+}
 
-    //<=============== SETTERS ================>//
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+    public static void main(String[] args) {
 
-    public void setContactName(String contactName) {
-        this.contactName = contactName;
+        userMenu();
+        captureUserInput();
+
+
     }
 
 }
+
+//TODO: Step 1 : Create the menu / options✅
+// TODO: Step 2: Capture user input✅
+// TODO: Step 3: Lets display something based on the option they choose
+
+// 5 minutes left? tf
+// just start another session after.aight im stuck

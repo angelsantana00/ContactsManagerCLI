86822e829b5c3bdd504360c37666278463aa7d95
Angel Santana <angel.d.santana00@gmail.com>
2022-03-13 21:02:52 -0500

    saving

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,86 +1,132 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
-public class Contacts {
+import static java.nio.file.Files.readAllLines;
 
-    //<============ Displays CLI =============>//
-    public static void userMenu() {
-        System.out.println("1. View Contacts \n" +
-                "2. Add a New Contact\n" +
-                "3. Search For Contact\n" +
-                "4. Delete Existing Contact\n" +
-                "5. Exit\n" +
-                "Please Enter an Option | 1 - 5 |");
+class ContactManager {
 
+    public static void displayMenu() throws IOException {
+        System.out.println("1. View contacts.");
+        System.out.println("2. Add a new contact.");
+        System.out.println("3. Search a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit.");
+        System.out.print("Enter an option (1, 2, 3, 4 or 5):");
         userOptions();
     }
 
-    //<============ User Input =============>//
-    public static int captureUserInput() {
-        Scanner in = new Scanner(System.in);
 
-        int usersChoice = in.nextInt();
-
-        if (usersChoice >= 1 && usersChoice <= 5) {
-            return usersChoice;
+    public static int userInput() {
+        Scanner readInput = new Scanner(System.in);
+        int userInput = readInput.nextInt();
+        if (userInput >= 1 && userInput <= 5) {
+            return userInput;
         } else {
-            System.out.println("Please Pick Another Number");
-            return captureUserInput();
+            System.out.println("Please choose A valid number.");
+            return userInput();
+        }
+
+    }
+
+    public static void createFile(String contact) throws IOException {
+        Files.write(
+                Paths.get("data", "info.txt"),
+                List.of(contact),
+                StandardOpenOption.APPEND
+        );
+
+    }
+
+    public static String retrieveNewContact() {
+        Scanner readInput = new Scanner(System.in);
+        System.out.print("Enter first name: ");
+        String firstName = readInput.next();
+        System.out.print("Enter Last name: ");
+        String lastName = readInput.next();
+        System.out.print("Enter Phone Number: ");
+        long phoneNumber = readInput.nextLong();
+        return firstName + lastName + phoneNumber;
+
+    }
+
+    public static void getContacts() throws IOException {
+        Path contactsPath = Paths.get("data", "info.txt");
+        List<String> contactList = Files.readAllLines(contactsPath);
+
+        for (int i = 0; i < contactList.size(); i += 1) {
+            System.out.println((i + 1) + ": " + contactList.get(i));
         }
     }
 
-    //<============ userOptions =============>//
-    public static void userOptions() {
+    public static void search(String query) throws IOException {
+        Path contactsPath = Paths.get("data", "info.txt");
+        List<String> contactList = Files.readAllLines(contactsPath);
+
+        for (String element : contactList) {
+            if(element.contains(query)){
+                System.out.println(element);
+            }
+        }
+
+    }
+
+    public static String getSearchInput(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter First Name: ");
+        return in.nextLine();
+    }
+
+    public static void deleteContact(String query) throws IOException {
+        Path contactsPath = Paths.get("data", "info.txt");
+        List<String> contactList = Files.readAllLines(contactsPath);
+
+        for (String element : contactList) {
+            if(element.contains(query)){
+                contactList.remove(element);
+
+                System.out.println(query + "Has been deleted");
+            }
+        }
+
+    }
+
+    public static void userOptions() throws IOException {
         int choice;
         do {
-            choice = captureUserInput();
-            if (choice == 1) {
-                System.out.println(" 1. Viewing all Contacts ");
+            choice = userInput();
+            switch (choice) {
+                case 1:getContacts();
+                    System.out.println("Viewing all Contacts");
+                    continue;
+                case 2:
+                    createFile(retrieveNewContact());
+                    displayMenu();
+                    continue;
+                case 3:search(getSearchInput());
+                    System.out.println("Search by name");
+                    displayMenu();
+                    continue;
+                case 4:deleteContact(getSearchInput());
+                    System.out.println("Delete");
+                    displayMenu();
 
-            } else if (choice == 2) {
-                System.out.println(" 2. Add a Contact ");
-
-            } else if (choice == 3) {
-                System.out.println(" 3. Search for Contact ");
-
-            } else if (choice == 4) {
-                System.out.println(" 4. Delete a Contact ");
             }
+
         } while (choice != 5);
-        System.out.println(" Exiting... ");
-    }
-
-    public static void viewContacts() {
-
-    }
-
-    public static void addContacts() {
-        System.out.println("Name Contact: ");
-
-
-    }
-
-    public static void searchContacts() {
-
-    }
-
-    public static void deleteContacts() {
-
+        System.out.println("Goodbye");
     }
 
 
-
-
-    public static void main(String[] args) {
-        userMenu();
+    public static void main(String[] args) throws IOException {
+        displayMenu();
 
     }
-
 }
-
-//TODO: Step 1 : Create the menu / options✅
-// TODO: Step 2: Capture user input✅
-// TODO: Step 3: Lets display something based on the option they choose
-// so we can already capture their input.... if a user picks 1 display "this"; just try writing the method
-// think how can we do that
-
-//  no ...what happens when u run this. it works BRB aight
